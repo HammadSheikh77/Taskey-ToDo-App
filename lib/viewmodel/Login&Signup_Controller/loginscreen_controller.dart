15632e104bbeaff3d0d907aabd6d84921b671976
MuhammadHammad77 <hammadop74@gmail.com>
2024-07-10 20:36:04 +0500

    Taskey app using GetX Statemanagment & firebase with MVVM architecture

--- lib/viewmodel/Login&Signup_Controller/loginscreen_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-
-class LoginscreenController extends GetxController{
-  final emailController = Rx(TextEditingController())().obs;
-  final passwordController = TextEditingController().obs;
-
-
-}
